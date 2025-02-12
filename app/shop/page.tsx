32b8bf98f2d67d7c281317eee67b62a312019f7d
RenzGH28 <renz4you28@gmail.com>
2025-02-13 05:43:56 +0700

    Penambahan Halaman Shop (Test)

---
+++ app/shop/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { useState } from "react"
+import { motion } from "framer-motion"
+import { Code2, Award, Layers } from "lucide-react"
+import { Header } from "@/components/header"
+import { Navbar } from "@/components/navbar"
+import { cn } from "@/lib/utils"
+
+export function ShopPage() {
+  return (
+    <div className="text-white">
+      <h1>Shop Page</h1>
+      <p>Ini halaman shop kamu.</p>
+    </div>
+  );
+}
