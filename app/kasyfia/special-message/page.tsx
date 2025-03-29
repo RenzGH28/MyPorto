d0e67987eea76acd6aa0633b48333af1db9a4f53
RenzGH28 <138320034+RenzGH28@users.noreply.github.com>
2025-03-30 04:30:34 +0700

    Add files via upload

---
+++ app/kasyfia/special-message/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
+      <iframe
+        src="/pesanuntukmu.html"
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "none",
+        }}
+      />
+    </div>
+  );
+}
